4e4af77ba04348a553122931d3bd911dd766f9c9
Ugur <ugur.ozdemir@ombori.com>
2022-05-12 15:46:06 +0300

    update

--- hiddenConnect.sh
+++ hiddenConnect.sh
@@ -2,5 +2,5 @@
 echo "Start killing wpa processes"
 sudo killall wpa_supplicant
 sleep 5
-echo "starting wpa_supplicant"
+echo "starting wpa_supplicant for $1"
 sudo wpa_supplicant -i $1 -c/etc/wpa_supplicant/wpa_supplicant-Hidden.conf -B
